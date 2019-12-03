09a2f14d00ad6c4737b6fdab2a54c14898e4c0df
LiZongwei <g0970216929@gmail.com>
2019-12-03 14:42:45 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <qrcode-stream @init="onInit" @decode="onDecode" :camera="cameraSettings" :track="false" :paused="paused"></qrcode-stream>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  data() {
+    return {
+      cameraSettings: {
+        audio: false,
+        video: {
+          facingMode: { ideal: "environment" }
+        }
+      },
+      paused: false
+    };
+  },
+  methods: {
+    onDecode(decodedString) {
+      alert(decodedString)
+    },
+    async onInit (promise) {
+    // show loading indicator
+
+    try {
+      await promise
+
+      // successfully initialized
+    } catch (error) {
+      if (error.name === 'NotAllowedError') {
+        // user denied camera access permisson
+      } else if (error.name === 'NotFoundError') {
+        // no suitable camera device installed
+      } else if (error.name === 'NotSupportedError') {
+        // page is not served over HTTPS (or localhost)
+      } else if (error.name === 'NotReadableError') {
+        // maybe camera is already in use
+      } else if (error.name === 'OverconstrainedError') {
+        // did you requested the front camera although there is none?
+      } else if (error.name === 'StreamApiNotSupportedError') {
+        // browser seems to be lacking features
+      }
+    } finally {
+      // hide loading indicator
+    }
   }
-}
+  }
+};
 </script>
